1ef18d4efb30d397582219ae84417d917943ccf4
Shivangi Mall <smdp365@gmail.com>
2024-02-11 01:08:03 +0530

    Responsive navbar

--- ims-frontend/src/layout/Navbar.jsx
+++ ims-frontend/src/layout/Navbar.jsx
@@ -1,35 +1,61 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "../css/Navbar.module.css"; // Import the CSS module
+import styles from "../css/Navbar.module.css";
 
 export default function Navbar() {
     return (
         <div>
             <nav
-                className={`navbar navbar-expand-sm fixed-top shadow ${styles.navbar}`}
+                className={`navbar navbar-expand-lg fixed-top shadow ${styles.navbar}`}
             >
                 <div className={`container ${styles.container}`}>
-                    <div className={styles.logoContainer}>
+                    <div className={`navbar-brand ${styles.logoContainer}`}>
                         <p className="text-center fs-2">VAAS</p>
                     </div>
 
-                    <Link className={`navbar-brand ${styles.brand}`} to={"/"}>
-                        Inventory Management System
-                    </Link>
+                    <ul className="navbar-nav">
+                        <li className="nav-item">
+                            <Link
+                                className={`nav-link ${styles.brand}`}
+                                to={"/"}
+                            >
+                                <h4>Inventory Management System</h4>
+                            </Link>
+                        </li>
+                    </ul>
 
-                    <Link
-                        className={`btn btn-outline-light ${styles.addButton}`}
-                        to="/addemployee"
+                    <button
+                        className="navbar-toggler"
+                        type="button"
+                        data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
                     >
-                        Add Employee
-                    </Link>
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
 
-                    <Link
-                        className={`btn btn-outline-light ${styles.loginButton}`}
-                        to="/adminlogin"
-                    >
-                        Admin Login
-                    </Link>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ms-auto">
+                            <li className="nav-item">
+                                <Link
+                                    className={`btn btn-outline-light ${styles.addButton}`}
+                                    to="/addemployee"
+                                >
+                                    Add Employee
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link
+                                    className={`btn btn-outline-light ${styles.loginButton}`}
+                                    to="/adminlogin"
+                                >
+                                    Admin Login
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
         </div>
